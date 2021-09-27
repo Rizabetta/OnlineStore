14bd66dbcc76f35f98b514dcc3c965dca78a41b5
Rizabetta <liza.popechiteleva@mail.ru>
2021-09-27 13:34:35 +0400

    connecting a database and writing to it

--- server/index.js
+++ server/index.js
@@ -1,3 +1,22 @@
+const { MongoClient } = require('mongodb')
+
+const client = new MongoClient('mongodb+srv://admin:mongodb@cluster0.7nzu9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+const start = async () => {
+    try {
+        await client.connect()
+        console.log("подключение установлено")
+        await client.db().createCollection('users')
+        const users = client.db().collection('users')
+        await users.insertOne({ name: 'ann', age: '15' })
+        const user = await users.findOne({ name: 'ann' })
+        console.log(user)
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
+
 require('dotenv').config()
 const express = require("express")
 
