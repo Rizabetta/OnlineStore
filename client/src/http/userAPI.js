866fce879f2a819b9c196a884048fb4fe0f53c38
Rizabetta <liza.popechiteleva@mail.ru>
2021-12-21 18:24:40 +0400

    communication between server and client

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -1,16 +1,22 @@
 import { $authHost, $host } from "./index";
+import jwt_decoder from "jwt-decode";
 
 export const registration = async (username, password) => {
-    const response = await $host.post('api/user/registration', { username, password, role: 'ADMIN' })
-    return response
+    const { data } = await $host.post('api/user/registration', { username, password, role: 'USER' })
+    localStorage.setItem('token', data.token)
+    console.log(data)
+    return jwt_decoder(data.token)
 }
 
 export const login = async (username, password) => {
-    const response = await $host.post('api/user/login', { username, password })
-    return response
+    const { data } = await $host.post('api/user/login', { username, password })
+    localStorage.setItem('token', data.token)
+
+    return jwt_decoder(data.token)
 }
 
 export const check = async () => {
-    const response = await $authHost.post('api/user/auth',)
-    return response
+    const { data } = await $authHost.get('api/user/auth')
+    localStorage.setItem('token', data.token)
+    return jwt_decoder(data.token)
 }
